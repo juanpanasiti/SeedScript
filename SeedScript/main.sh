afcd6e1399ddb9d58d15d865153f69743fb88ab2
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-02-12 00:32:26 -0300

    Creado el menú principal del script con 2 opciones principales tentativas

---
+++ SeedScript/main.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Script to do basic install
+
+option=0
+continue=true
+while $continue
+do
+  echo "\e[1;31mSelecciona una opción:\e[0m"
+  echo -n "\e[1;32m"
+  echo "\t1. Instalar programas"
+  echo "\t2. Ejecutar configuraciones"
+  echo -n "\e[1;31m"
+  echo "\t0. Salir"
+  echo -n "\e[1;33m"
+  read option
+  clear
+  case $option in
+    1 ) sh install_software.sh;;
+    2 ) echo "Ejecutar configuraciones";;
+    0 ) continue=false;;
+    * ) echo "Opcion incorrecta";;
+  esac
+  echo -n "\e[0m"
+done
