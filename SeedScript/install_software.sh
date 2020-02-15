2d630c545df7716fac6522fcc7ca9151a7674479
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-02-14 23:33:37 -0300

    Agregadas las opciones al menu de juegos

--- SeedScript/install_software.sh
+++ SeedScript/install_software.sh
@@ -20,8 +20,8 @@ main_menu()
     clear
     case $option in
       1 ) menu_dev_software;;
-      2 ) echo "Ejecutar configuraciones";;
-      3 ) echo "Ejecutar configuraciones";;
+      2 ) menu_multimedia_n_streaming;;
+      3 ) menu_games;;
       4 ) echo "Ejecutar configuraciones";;
       0 ) continue=false;;
       * ) echo "Opcion incorrecta";;
@@ -144,7 +144,7 @@ menu_dev_software()
   done
 }
 
-menu_multimedia_n_streaming
+menu_multimedia_n_streaming()
 {
   option=0
   continue=true
@@ -238,7 +238,62 @@ menu_multimedia_n_streaming
     esac
     echo -n "\e[0m"
   done
-}
+}#menu_multimedia_n_streaming()
+
+menu_games()
+{
+  option=0
+  continue=true
+  while $continue
+  do
+    echo "\e[1;31mSelecciona un juego a instalar:\e[0m"
+
+    #Steam (Flathub) option
+    echo -n "\e[0;94m\t1. Steam (Flathub)\e[0m"
+    echo ""
+    #check_soft "_____"
+
+    #SuperTuxKart option
+    echo -n "\e[0;94m\t2.  SuperTuxKart (Flathub)\e[0m"
+    echo ""
+    #check_soft "psensor"
+
+    #HexGL (Flathub) option
+    echo -n "\e[0;94m\t3. HexGL (Flathub)\e[0m"
+    echo ""
+    #check_soft "_____"
+
+    #Minetest (Flathub) option
+    echo -n "\e[0;94m\t4. Minetest (Flathub)\e[0m"
+    echo ""
+    #check_soft "_____"
+
+
+    #Billiards (Flathub) option
+    echo -n "\e[0;94m\t5. Billiards (Flathub)\e[0m"
+    echo ""
+    #check_soft "_____"
+
+    #_____ option
+    #echo -n "\e[0;94m\tn. _____\e[0m"
+    #echo ""
+    #check_soft "_____"
+
+    echo "\e[1;31m\t0. Salir\e[0m"
+    read option
+    clear
+    case $option in
+      1 ) sh install/super_tux_kart.sh;;#SuperTuxKart (Flathub)
+      2 ) sh install/steam.sh;;#Steam (Flathub)
+      3 ) sh install/minetest.sh;;#Minetest (Flathub)
+      4 ) sh install/billiards.sh;;#Billiards (Flathub)
+      5 ) sh install/hexgl.sh;;#HexGL (Flathub)
+      0 ) continue=false;;
+      * ) echo "Opcion incorrecta";;
+    esac
+    echo -n "\e[0m"
+  done
+}#menu_games
 ##################################
 #Inicio del script
 main_menu
