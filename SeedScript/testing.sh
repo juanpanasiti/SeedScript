d86324e05bd5d121a8a9f63633fe44ed10962038
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-02-14 23:34:56 -0300

    Eliminado archivo de pruebas de código

--- SeedScript/testing.sh
+++
@@ -1,9 +0,0 @@
-echo "El caracter de escape <esc> puede ser obtenido de las siguientes formas:"
-echo "  \e"
-echo "  \033"
-echo "  \x1b"
-echo "Se utiliza con el corchete [ seguido del código de formato (FormatCode) seguido de una m"
-printf "el formato general sería:"
-echo "  \e[FormatCodem"
-
-printf "\e[31m" "\e"  "FormatCode"
